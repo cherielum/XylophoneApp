7e0d1f3933d548ff6a291f4de03a0dac54b35493
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-15 14:51:42 -0400

    adding diff sounds

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -6,33 +6,43 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    var player: AVAudioPlayer?
+    var audioPlayer: AVAudioPlayer!
+    var selectedSoundFileName: String = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-      let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
         
-        //catching errors
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            self.player = sound
-            sound.numberOfLoops = 0
-            sound.prepareToPlay()
-            sound.play()
-            print("testing")
-        } catch {
-            print("error loading file")
-            // couldn't load file :(
-        }
+        //subtract one because array starts at 0. therefore note1 can be 0.
         
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        playSound()
     }
     
-  
+    func playSound() {
+        //sets up location of where sound is
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        //plays same sound
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+            
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+    
 
 }
 
